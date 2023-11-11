53e374641a0d9d2b0fc9ac281d10542a423e1fd9
Baz Rahimi <bazrahimi@hotmail.com>
2023-11-11 16:35:31 +1100

    add strategies

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,30 +1,47 @@
+ // importing various workbox strategies and utilities for service worker functionality.
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+
+// Precaching files that are part of the webpack build process
 precacheAndRoute(self.__WB_MANIFEST);
 
+// CacheFirst strategy for pages to quickly load previously visited pages.
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
+    // ensures only responses with cetian statuses are catched.
     new CacheableResponsePlugin({
       statuses: [0, 200],
     }),
+    // Automatically remove old cache entries.
     new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, // cache item for 30 days.
     }),
   ],
 });
-
+// Preload and cache pages to ensure they are available offline.
 warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
+  urls: ['/index.html', '/'], // urls to catch
+  strategy: pageCache, // Strategy to use
 });
-
+// Registering a route for navigation requests to sue the CacheFirst strategy.
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+
+registerRoute(
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: "asset-cache",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,3 +1,4 @@
+// importing necessary plugins and modules for webpack configuration
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -6,35 +7,61 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
-
+// configur webpack
 module.exports = () => {
   return {
     mode: 'development',
+    // entry points for the application
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+    // output congiguration for the bundle webpack will generate
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    // plugins used by webpack to generate HTML files, extract CSS into separate files, and inject the service worker.
     plugins: [
+      // Generate an HTML file from a template, includes webpack bundles in the generated HTML
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'Text Editor',
       }),
+      // extract CSS into separate files. it crate a CSS file per JS file which  contain CSS.
       new MiniCssExtractPlugin(),
+      // InjectManifest plugin for workbox will generate a service worker file that precache and route requests
       new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'service.worker.js',
+        swSrc: './src-sw.js', // specify the source service worker file
+        swDest: 'src-sw.js', // Destination filename in the ouptput directory.
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "J.A.T.E",
+        description: "offline text editor",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
       }),
 
       
     ],
-
+    // Module rules tell webpack how to handle different type of modules
     module: {
       rules: [
         {
+          // Rules for CSS file: use miniCssExtractPlugin to extract CSS into separate files.
           test: /\.css$/i,
           use: [MiniCssExtractPlugin.loader, 'css-loader'],
         },
